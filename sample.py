ef7101501d2376da5ef5d07feb689debeffa18a8
Raghavi27 <raghavi.devalla27@gmail.com>
2023-04-07 12:09:02 +0530

    sample file

---
+++ sample.py
@@ -0,0 +1,13 @@
+def check(s1, s2):
+    # the sorted strings are checked
+    if (sorted(s1) == sorted(s2)):
+        print("The strings are anagrams.")
+    else:
+        print("The strings aren't anagrams.")
+
+    # driver code
+
+
+s1 = input()
+s2 = input()
+check(s1, s2)
